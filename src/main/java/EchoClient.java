723827f67f983a921dcf9dc61d1035d836188345
ajin0214 <njia214@gmail.com>
2023-12-27 18:03:46 +0900

    에코서버와 클라이언트 기본

---
+++ src/main/java/EchoClient.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+import java.util.Objects;
+
+public class EchoClient {
+    public static void main(String[] args){
+        String hostName = "localhost";
+        int portNumber = 8080;
+
+        try (Socket echoSocket = new Socket(hostName, portNumber);
+             PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String userInput;
+
+            while (!Objects.equals(userInput = stdIn.readLine(), "exit")) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
+    }
+}
