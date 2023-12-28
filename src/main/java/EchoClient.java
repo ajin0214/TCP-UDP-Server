bd24810190e1074beb245c6074895035c9cf0f82
ajin0214 <njia214@gmail.com>
2023-12-28 14:43:17 +0900

    udp 완료

--- src/main/java/EchoClient.java
+++ src/main/java/EchoClient.java
@@ -1,20 +1,29 @@
 import java.io.*;
 import java.net.*;
-import java.util.Objects;
 
 public class EchoClient {
-    public static void main(String[] args){
-        String hostName = "localhost";
-        int portNumber = 8080;
-
-        if (args.length > 0) {
-            try {
-                portNumber = Integer.parseInt(args[0]);
-            } catch (NumberFormatException ne) {
-                System.err.println("Argument must be an integer");
-                System.exit(1);
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            switch (args[0]) {
+                case "tcp":
+                    tcpClient(args[1]);
+                    break;
+                case "udp":
+                    udpClient(args[1]);
+                    break;
+                default:
+                    System.out.println("Invalid argument");
             }
+        } else {
+            System.out.println("No arguments");
         }
+    }
+
+    public static void tcpClient(String arg){
+        System.out.println("TCP Client Test");
+        String hostName = "localhost";
+        int portNumber = Integer.parseInt(arg);
+
         try {
             Socket echoSocket = new Socket(hostName, portNumber);
             System.out.println("port : " + portNumber);
@@ -25,7 +34,7 @@ public class EchoClient {
             String input;
             while ((userInput = stdIn.readLine())!= null) {
                 out.println(userInput);
-                System.out.println("echo: " + (input = in.readLine()));
+                System.out.println("echo : " + (input = in.readLine()));
                 if (input.equals("exit")){
                     break;
                 }
@@ -44,6 +53,41 @@ public class EchoClient {
             System.err.println("Couldn't get I/O for the connection to " + hostName);
             System.err.println(ie.getMessage());
             System.exit(1);
+        }
+    }
+
+    public static void udpClient(String arg){
+        System.out.println("UDP Client Test");
+        int portNumber = Integer.parseInt(arg);
+        System.out.println("port : " + portNumber);
+
+        try {
+            InetAddress address = InetAddress.getByName("localhost");
+            DatagramSocket socket = new DatagramSocket();
+
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            String userInput;
+
+            while((userInput = stdIn.readLine())!= null){
+                byte[] buf = userInput.getBytes();
+                DatagramPacket packet = new DatagramPacket(buf, buf.length, address, portNumber);
+
+                socket.send(packet);
+
+                packet = new DatagramPacket(buf,buf.length);
+                socket.receive(packet);
+
+                String echo = new String(packet.getData(),0, packet.getLength());
+                System.out.println("echo : " + echo);
+                if (userInput.equals("exit")){
+                    break;
+                }
+
+            }
+
+
+        } catch (IOException ie) {
+            throw new RuntimeException(ie);
         }
     }
 }
--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -2,28 +2,39 @@ import java.io.*;
 import java.net.*;
 
 public class EchoServer {
-    public static void main(String[] args){
-        int portNumber = 8080;
-        if (args.length > 0) {
-            try {
-                portNumber = Integer.parseInt(args[0]);
-            } catch (NumberFormatException ne) {
-                System.err.println("Argument must be an integer");
-                System.exit(1);
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            switch (args[0]) {
+                case "tcp":
+                    tcpServer(args[1]);
+                    break;
+                case "udp":
+                    udpServer(args[1]);
+                    break;
+                default:
+                    System.out.println("Invalid argument");
             }
+        } else {
+            System.out.println("No arguments");
         }
+    }
+
+    public static void tcpServer(String arg){
+        System.out.println("TCP Server Test");
+        int portNumber = Integer.parseInt(arg);
+        System.out.println("port : " + portNumber);
 
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
-            System.out.println("서버준비완료");
+            System.out.println("Server is ready");
             Socket clientSocket = serverSocket.accept();
-            System.out.println("클라이언트연결완료");
+            System.out.println("Connect completed");
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String inputLine;
 
             while ((inputLine = in.readLine()) != null) {
-                System.out.println("Client Receive : " + inputLine);
+                System.out.println("Client>" + inputLine);
                 out.println(inputLine);
                 if (inputLine.equals("exit")){
                     break;
@@ -40,4 +51,41 @@ public class EchoServer {
             System.err.println(ie.getMessage());
         }
     }
+
+    public static void udpServer(String arg) {
+        System.out.println("UDP Server Test");
+        int portNumber = Integer.parseInt(arg);
+        System.out.println("port : " + portNumber);
+
+        try{
+            DatagramSocket socket = new DatagramSocket(portNumber);
+            while(true){
+                byte[] buf = new byte[100];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+                socket.receive(packet);
+                String dataGot = new String(packet.getData(),0, packet.getLength());
+                System.out.println("Client>" + dataGot);
+                InetAddress address =packet.getAddress();
+                int clientPort = packet.getPort();
+
+                packet = new DatagramPacket(buf, buf.length, address,clientPort);
+
+                socket.send(packet);
+
+                if (dataGot.equals("exit")){
+                    break;
+                }
+
+
+
+            }
+
+        } catch (IOException ie) {
+            System.err.println(ie.getMessage());
+        }
+
+    }
+
+
 }
